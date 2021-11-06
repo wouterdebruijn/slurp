5158dd57359b8babecb2743c3ba8bc1e96060926
Wouter de Bruijn <wouter@hedium.nl>
2021-11-06 15:38:05 +0100

    :tada: Started the project :partying_face:

---
+++ src/main/java/dev/krijninc/slurp/Slurp.java
@@ -0,0 +1,17 @@
+package dev.krijninc.slurp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Slurp extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}

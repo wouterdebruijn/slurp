506322d726517631b0e41b7101d3f883a38377f8
Wouter de Bruijn <wouter@hedium.nl>
2021-11-06 21:19:05 +0100

    :sparkles: Added config with loader

---
+++ src/main/java/dev/krijninc/slurp/ConfigLoader.java
@@ -0,0 +1,32 @@
+package dev.krijninc.slurp;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ConfigLoader {
+    private static FileConfiguration config;
+
+    // TODO: Add enum of command keys that exist.
+
+    public static void saveDefaults() {
+        JavaPlugin plugin = Slurp.getPlugin();
+        plugin.saveDefaultConfig();
+    }
+
+    public static void load() {
+        JavaPlugin plugin = Slurp.getPlugin();
+        config = plugin.getConfig();
+    }
+
+    public static String getString(String key) {
+        return config.getString(key);
+    }
+
+    public static Boolean getBoolean(String key) {
+        return config.getBoolean(key);
+    }
+
+    public static int getInt(String key) {
+        return config.getInt(key);
+    }
+}

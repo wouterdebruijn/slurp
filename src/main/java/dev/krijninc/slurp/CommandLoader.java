6aafe7ef8b37d40f5a0c389df47fd2d76fe7de9a
Wouter de Bruijn <wouter@hedium.nl>
2021-11-06 21:19:35 +0100

    :sparkles: Added commandloader

---
+++ src/main/java/dev/krijninc/slurp/CommandLoader.java
@@ -0,0 +1,19 @@
+package dev.krijninc.slurp;
+
+import dev.krijninc.slurp.commands.*;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
+
+public class CommandLoader {
+
+    static void load() {
+        JavaPlugin plugin = Slurp.getPlugin();
+
+        Objects.requireNonNull(plugin.getCommand("giveshot")).setExecutor(new GiveShot());
+        Objects.requireNonNull(plugin.getCommand("giveSip")).setExecutor(new GiveSip());
+        Objects.requireNonNull(plugin.getCommand("stats")).setExecutor(new Stats());
+        Objects.requireNonNull(plugin.getCommand("takeshot")).setExecutor(new TakeShot());
+        Objects.requireNonNull(plugin.getCommand("takesip")).setExecutor(new TakeSip());
+    }
+}

b34de9eac3e6e565a7f3de483ac9857cc63744dc
Wouter de Bruijn <wouter@hedium.nl>
2023-06-22 22:16:49 +0200

    🔥 Cleaned up debug command

--- src/main/java/nl/wouterdebruijn/slurp/command/session/Debug.java
+++ src/main/java/nl/wouterdebruijn/slurp/command/session/Debug.java
@@ -2,7 +2,9 @@ package nl.wouterdebruijn.slurp.command.session;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import net.kyori.adventure.text.Component;
 import nl.wouterdebruijn.slurp.helper.game.entity.SlurpSession;
+import nl.wouterdebruijn.slurp.helper.game.handlers.TitleCountdownHandler;
 import nl.wouterdebruijn.slurp.helper.game.manager.SlurpSessionManager;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -18,32 +20,6 @@ public class Debug implements TabExecutor {
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         Player player = (Player) sender;
 
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-
-//        Print all current sessions
-//        player.sendMessage("Current players:");
-//        for (SlurpPlayer slurpPlayer : SlurpPlayerManager.dump()) {
-//            player.sendMessage(gson.toJson(slurpPlayer));
-//            player.sendMessage("-------------");
-//        }
-
-//        player.sendMessage("Current sessions:");
-        for (SlurpSession slurpPlayer : SlurpSessionManager.dump()) {
-            player.sendMessage(gson.toJson(slurpPlayer));
-            player.sendMessage("-------------");
-        }
-
-//        Slurp.logger.log(Level.INFO, "Current players:");
-//        for (SlurpPlayer slurpPlayer : SlurpPlayerManager.dump()) {
-//            Slurp.logger.log(Level.INFO, gson.toJson(slurpPlayer));
-//            Slurp.logger.log(Level.INFO, "-------------");
-//        }
-//
-//        Slurp.logger.log(Level.INFO, "Current sessions:");
-//        for (SlurpSession slurpPlayer : SlurpSessionManager.dump()) {
-//            Slurp.logger.log(Level.INFO, gson.toJson(slurpPlayer));
-//            Slurp.logger.log(Level.INFO, "-------------");
-//        }
         return true;
     }
 

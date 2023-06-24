0ae6c34a1a1a55479c14c0be2cef6be4d0843be2
Wouter de Bruijn <wouter@hedium.nl>
2023-06-25 00:54:18 +0200

    💡Added basic documentation to entry class

--- src/main/java/nl/wouterdebruijn/slurp/helper/game/api/SlurpEntryBuilder.java
+++ src/main/java/nl/wouterdebruijn/slurp/helper/game/api/SlurpEntryBuilder.java
@@ -11,6 +11,16 @@ public class SlurpEntryBuilder {
     private boolean giveable;
     private boolean transfer;
 
+    /**
+     * Create entry to change slurp data
+     *
+     * @param sips Sips to add or remove. Positive values will add, negative values will subtract.
+     * @param shots Shots to add or remove. Postive values will add, negative values will subtract.
+     * @param player Player UUID whom the entry is meant for.
+     * @param session UUID of the session of the player whom the entry is for.
+     * @param giveable True if the sips can be given away, false if not.
+     * @param transfer True if the sips are transferred to another player, false if not.
+     */
     public SlurpEntryBuilder(int sips, int shots, String player, String session, boolean giveable, boolean transfer) {
         this.sips = sips;
         this.shots = shots;

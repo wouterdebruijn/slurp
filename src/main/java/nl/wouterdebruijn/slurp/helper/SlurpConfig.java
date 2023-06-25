d00f0710ac03cc026b03b0fdb69064cd75c79424
Dani Baas <Dani.raj25@gmail.com>
2023-06-26 01:39:41 +0200

    ✨ Setting, getting values and saving the config

--- src/main/java/nl/wouterdebruijn/slurp/helper/SlurpConfig.java
+++ src/main/java/nl/wouterdebruijn/slurp/helper/SlurpConfig.java
@@ -1,11 +1,13 @@
 package nl.wouterdebruijn.slurp.helper;
 
 import nl.wouterdebruijn.slurp.Slurp;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.Plugin;
 
 public class SlurpConfig {
     public static void initialize() {
         Plugin plugin = Slurp.plugin;
+        setValues();
         plugin.saveDefaultConfig();
     }
 
@@ -23,5 +25,94 @@ public class SlurpConfig {
 
     public static String prefix() {
         return getString("chat.prefix");
+    }
+
+    private static void setValues() {
+        FileConfiguration conf = Slurp.plugin.getConfig();
+        if (!conf.contains(ConfigValue.CHANCES_LUCYSTONE.getPath())) { // chances
+            conf.set(ConfigValue.CHANCES_LUCYSTONE.getPath(), 1000000);
+        } else if (!conf.contains(ConfigValue.CHANCES_BURNING_HANDS.getPath())) {
+            conf.set(ConfigValue.CHANCES_BURNING_HANDS.getPath(), 1000000);
+        } else if (!conf.contains(ConfigValue.CHANCES_CHOKING.getPath())) {
+            conf.set(ConfigValue.CHANCES_CHOKING.getPath(), 1000000);
+        } else if (!conf.contains(ConfigValue.CHANCES_FALL_DAMAGE.getPath())) {
+            conf.set(ConfigValue.CHANCES_FALL_DAMAGE.getPath(), 1000000);
+        } else if (!conf.contains(ConfigValue.CHANCES_SPRINTING.getPath())) {
+            conf.set(ConfigValue.CHANCES_SPRINTING.getPath(), 1000000);
+        } else if (!conf.contains(ConfigValue.CHANCES_SWIMMING.getPath())) {
+            conf.set(ConfigValue.CHANCES_SWIMMING.getPath(), 1000000);
+        } else if (!conf.contains(ConfigValue.CHANCES_BOAT.getPath())) {
+            conf.set(ConfigValue.CHANCES_BOAT.getPath(), 1000000);
+        } else if (!conf.contains(ConfigValue.SIP_LUCYSTONE.getPath())) { // sips
+            conf.set(ConfigValue.SIP_LUCYSTONE.getPath(), 0);
+        } else if (!conf.contains(ConfigValue.SIP_BURNING_HANDS.getPath())) {
+            conf.set(ConfigValue.SIP_BURNING_HANDS.getPath(), 5);
+        } else if (!conf.contains(ConfigValue.SIP_CHOKING.getPath())) {
+            conf.set(ConfigValue.SIP_CHOKING.getPath(), 4);
+        } else if (!conf.contains(ConfigValue.SIP_FALL_DAMAGE.getPath())) {
+            conf.set(ConfigValue.SIP_FALL_DAMAGE.getPath(), 2);
+        } else if (!conf.contains(ConfigValue.SIP_DIE.getPath())) {
+            conf.set(ConfigValue.SIP_DIE.getPath(), 2);
+        } else if (!conf.contains(ConfigValue.SIP_KILLER.getPath())) {
+            conf.set(ConfigValue.SIP_KILLER.getPath(), 0);
+        } else if (!conf.contains(ConfigValue.SIP_KILLED_ANIMAL.getPath())) {
+            conf.set(ConfigValue.SIP_KILLED_ANIMAL.getPath(), 3);
+        } else if (!conf.contains(ConfigValue.SIP_SPRINTING.getPath())) {
+            conf.set(ConfigValue.SIP_SPRINTING.getPath(), 2);
+        } else if (!conf.contains(ConfigValue.SIP_SWIMMING.getPath())) {
+            conf.set(ConfigValue.SIP_SWIMMING.getPath(), 2);
+        } else if (!conf.contains(ConfigValue.SIP_BOAT.getPath())) {
+            conf.set(ConfigValue.SIP_BOAT.getPath(), 3);
+        } else if (!conf.contains(ConfigValue.SHOT_LUCYSTONE.getPath())) { //shots
+            conf.set(ConfigValue.SHOT_LUCYSTONE.getPath(), 1);
+        } else if (!conf.contains(ConfigValue.SHOT_BURNING_HANDS.getPath())) {
+            conf.set(ConfigValue.SHOT_BURNING_HANDS.getPath(), 0);
+        } else if (!conf.contains(ConfigValue.SHOT_CHOKING.getPath())) {
+            conf.set(ConfigValue.SHOT_CHOKING.getPath(), 0);
+        } else if (!conf.contains(ConfigValue.SHOT_FALL_DAMAGE.getPath())) {
+            conf.set(ConfigValue.SHOT_FALL_DAMAGE.getPath(), 0);
+        } else if (!conf.contains(ConfigValue.SHOT_DIE.getPath())) {
+            conf.set(ConfigValue.SHOT_DIE.getPath(), 0);
+        } else if (!conf.contains(ConfigValue.SHOT_KILLER.getPath())) {
+            conf.set(ConfigValue.SHOT_KILLER.getPath(), 1);
+        } else if (!conf.contains(ConfigValue.SHOT_KILLED_ANIMAL.getPath())) {
+            conf.set(ConfigValue.SHOT_KILLED_ANIMAL.getPath(), 0);
+        } else if (!conf.contains(ConfigValue.SHOT_SPRINTING.getPath())) {
+            conf.set(ConfigValue.SHOT_SPRINTING.getPath(), 0);
+        } else if (!conf.contains(ConfigValue.SHOT_SWIMMING.getPath())) {
+            conf.set(ConfigValue.SHOT_SWIMMING.getPath(), 0);
+        } else if (!conf.contains(ConfigValue.SHOT_BOAT.getPath())) {
+            conf.set(ConfigValue.SHOT_BOAT.getPath(), 0);
+        }
+    }
+
+    /**
+     * Returns the value from the config. Only applicable to Chance, Sips or Shots
+     *
+     * @param val ConfigValue's data to be retrieved from the config.
+     * @return Returns integer value stored in config.
+     */
+    public static int getValue(ConfigValue val) {
+        FileConfiguration conf = Slurp.plugin.getConfig();
+        if (conf.contains(val.getPath())) {
+            return conf.getInt(val.getPath());
+        }
+        return 0;
+    }
+
+    /**
+     * Set a value in the config. Only applicable to Chance, Sips or Shots
+     *
+     * @param val ConfigValue's data to be stored in the config.
+     * @param value Integer value to be stored in the config.
+     */
+    public static void setValue(ConfigValue val, int value) {
+        FileConfiguration conf = Slurp.plugin.getConfig();
+        conf.set(val.getPath(), value);
+        saveConfig();
+    }
+
+    public static void saveConfig() {
+        Slurp.getPlugin(Slurp.class).saveConfig();
     }
 }

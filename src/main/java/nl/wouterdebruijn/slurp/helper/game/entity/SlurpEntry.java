83c677f19ac0c9abe5758e0406eecbb40252f4b6
Dani Baas <Dani.raj25@gmail.com>
2023-06-25 23:27:46 +0200

    🧑‍💻 Switches from callback to CompletableFuture

--- src/main/java/nl/wouterdebruijn/slurp/helper/game/entity/SlurpEntry.java
+++ src/main/java/nl/wouterdebruijn/slurp/helper/game/entity/SlurpEntry.java
@@ -2,13 +2,11 @@ package nl.wouterdebruijn.slurp.helper.game.entity;
 
 import com.google.gson.Gson;
 import nl.wouterdebruijn.slurp.Slurp;
-import nl.wouterdebruijn.slurp.exceptions.ApiResponseException;
 import nl.wouterdebruijn.slurp.helper.SlurpConfig;
 import nl.wouterdebruijn.slurp.helper.game.api.ResponseEntry;
 import nl.wouterdebruijn.slurp.helper.game.api.SlurpEntryBuilder;
 import nl.wouterdebruijn.slurp.helper.game.manager.DrinkingBuddyManager;
 import nl.wouterdebruijn.slurp.helper.game.manager.SlurpPlayerManager;
-import org.bukkit.Bukkit;
 
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -16,7 +14,7 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.concurrent.CompletableFuture;
-import java.util.function.Consumer;
+import java.util.concurrent.ExecutionException;
 import java.util.logging.Level;
 
 public class SlurpEntry {
@@ -37,31 +35,31 @@ public class SlurpEntry {
         this.transfer = transfer;
     }
 
-    public static void createDirect(SlurpEntryBuilder entry, String token, Consumer<SlurpEntry> callback) {
-        Bukkit.getScheduler().runTaskAsynchronously(Slurp.plugin, () -> {
-            try {
-                Gson gson = new Gson();
+    public static CompletableFuture<SlurpEntry> createDirect(SlurpEntryBuilder entry, String token) {
+        try {
+            Gson gson = new Gson();
 
-                HttpRequest request = HttpRequest.newBuilder()
-                        .uri(new URI(API_URL + "/entry"))
-                        .POST(HttpRequest.BodyPublishers.ofString(gson.toJson(entry)))
-                        .header("Content-Type", "application/json")
-                        .header("Authorization", "Bearer " + token)
-                        .build();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI(API_URL + "/entry"))
+                    .POST(HttpRequest.BodyPublishers.ofString(gson.toJson(entry)))
+                    .header("Content-Type", "application/json")
+                    .header("Authorization", "Bearer " + token)
+                    .build();
 
-                HttpClient client = HttpClient.newHttpClient();
-                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            HttpClient client = HttpClient.newHttpClient();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-                if (response.statusCode() != 200) {
-                    throw new ApiResponseException(request, response);
-                }
-
-                ResponseEntry responseEntry = gson.fromJson(response.body(), ResponseEntry.class);
-                callback.accept(responseEntry.toSlurpEntry());
-            } catch (Exception e) {
-                Slurp.logger.log(Level.SEVERE, "Error while creating entry", e);
+            if (response.statusCode() != 200) {
+                //throw new ApiResponseException(request, response);
+                return CompletableFuture.failedFuture(new Exception("API exception"));
             }
-        });
+            ResponseEntry responseEntry = gson.fromJson(response.body(), ResponseEntry.class);
+            //callback.accept(responseEntry.toSlurpEntry());
+            return CompletableFuture.completedFuture(responseEntry.toSlurpEntry());
+        } catch (Exception e) {
+            Slurp.logger.log(Level.SEVERE, "Error while creating entry", e);
+            return CompletableFuture.failedFuture(new Exception("Error while creating entry"));
+        }
     }
 
     public static CompletableFuture<ArrayList<SlurpEntry>> create(SlurpEntryBuilder entry, String token) {
@@ -80,17 +78,19 @@ public class SlurpEntry {
         ArrayList<SlurpEntry> entries = new ArrayList<>();
 
         // Create entry for player
-        createDirect(entry, token, entries::add);
+        try {
+            entries.add(createDirect(entry, token).get());
 
-        // If the player has buddies create entries for them too
-        if (buddies != null && entry.shouldTransferToDrinkingBuddies()) {
-            // Create entry for each buddy
-
-            for (SlurpPlayer buddy : buddies) {
-                createDirect(entry.copyForPlayer(buddy), token, entries::add);
+            // If the player has buddies create entries for them too
+            if (buddies != null && entry.shouldTransferToDrinkingBuddies()) {
+                // Create entry for each buddy
+                for (SlurpPlayer buddy : buddies) {
+                    entries.add(createDirect(entry.copyForPlayer(buddy), token).get());
+                }
             }
+        } catch (InterruptedException|ExecutionException e) {
+            return CompletableFuture.failedFuture(e);
         }
-
         return CompletableFuture.completedFuture(entries);
     }
 }

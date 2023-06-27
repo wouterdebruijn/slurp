a2d0cb643f7af9555f75fbdbe12792ae77b2cbb7
Dani Baas <Dani.raj25@gmail.com>
2023-06-27 20:17:09 +0200

    🧑‍💻 Changed structure of ConfigValue so handling with config is less tedious

--- src/main/java/nl/wouterdebruijn/slurp/helper/ConfigValue.java
+++ src/main/java/nl/wouterdebruijn/slurp/helper/ConfigValue.java
@@ -4,41 +4,47 @@ import org.jetbrains.annotations.Nullable;
 
 public enum ConfigValue {
 
-    CHANCES_LUCYSTONE("chances.lucyStone"),
-    CHANCES_BURNING_HANDS("chances.burnHands"),
-    CHANCES_CHOKING("chances.choking"),
-    CHANCES_FALL_DAMAGE("chances.fallDamage"),
-    CHANCES_SPRINTING("chances.sprintingChance"),
-    CHANCES_SWIMMING("chances.swimmingChance"),
-    CHANCES_BOAT("chances.boatChance"),
-    SIP_LUCYSTONE("drinks.sips.lucyStone"),
-    SIP_BURNING_HANDS("drinks.sips.burnHands"),
-    SIP_CHOKING("drinks.sips.choking"),
-    SIP_FALL_DAMAGE("drinks.sips.fallDamage"),
-    SIP_DIE("drinks.sips.die"),
-    SIP_KILLER("drinks.sips.killer"),
-    SIP_KILLED_ANIMAL("drinks.sips.killAnimal"),
-    SIP_SPRINTING("drinks.sips.sprinting"),
-    SIP_SWIMMING("drinks.sips.swimming"),
-    SIP_BOAT("drinks.sips.boat"),
-    SHOT_LUCYSTONE("drinks.shots.lucyStone"),
-    SHOT_BURNING_HANDS("drinks.shots.burnHands"),
-    SHOT_CHOKING("drinks.shots.choking"),
-    SHOT_FALL_DAMAGE("drinks.shots.fallDamage"),
-    SHOT_DIE("drinks.shots.die"),
-    SHOT_KILLER("drinks.shots.killer"),
-    SHOT_KILLED_ANIMAL("drinks.shots.killAnimal"),
-    SHOT_SPRINTING("drinks.shots.sprinting"),
-    SHOT_SWIMMING("drinks.shots.swimming"),
-    SHOT_BOAT("drinks.shots.boat");
+    CHANCES_LUCYSTONE("chances.lucyStone", 1000000),
+    CHANCES_BURNING_HANDS("chances.burnHands", 1000000),
+    CHANCES_CHOKING("chances.choking", 1000000),
+    CHANCES_FALL_DAMAGE("chances.fallDamage", 1000000),
+    CHANCES_SPRINTING("chances.sprintingChance", 1000000),
+    CHANCES_SWIMMING("chances.swimmingChance", 1000000),
+    CHANCES_BOAT("chances.boatChance", 1000000),
+    SIP_LUCYSTONE("drinks.sips.lucyStone", 0),
+    SIP_BURNING_HANDS("drinks.sips.burnHands", 0),
+    SIP_CHOKING("drinks.sips.choking", 4),
+    SIP_FALL_DAMAGE("drinks.sips.fallDamage", 2),
+    SIP_DIE("drinks.sips.die", 2),
+    SIP_KILLER("drinks.sips.killer", 2),
+    SIP_KILLED_ANIMAL("drinks.sips.killAnimal", 3),
+    SIP_SPRINTING("drinks.sips.sprinting", 2),
+    SIP_SWIMMING("drinks.sips.swimming", 2),
+    SIP_BOAT("drinks.sips.boat", 3),
+    SHOT_LUCYSTONE("drinks.shots.lucyStone", 1),
+    SHOT_BURNING_HANDS("drinks.shots.burnHands", 0),
+    SHOT_CHOKING("drinks.shots.choking", 0),
+    SHOT_FALL_DAMAGE("drinks.shots.fallDamage", 0),
+    SHOT_DIE("drinks.shots.die", 0),
+    SHOT_KILLER("drinks.shots.killer", 1),
+    SHOT_KILLED_ANIMAL("drinks.shots.killAnimal", 0),
+    SHOT_SPRINTING("drinks.shots.sprinting", 0),
+    SHOT_SWIMMING("drinks.shots.swimming", 0),
+    SHOT_BOAT("drinks.shots.boat", 0);
 
     private final String confPath;
+    private final int value;
 
-    ConfigValue(String confPath) {
+    ConfigValue(String confPath, int value) {
         this.confPath = confPath;
+        this.value = value;
     }
 
     public String getPath() {
         return confPath;
+    }
+
+    public int getDefaultValue() {
+        return value;
     }
 }
--- src/main/java/nl/wouterdebruijn/slurp/helper/SlurpConfig.java
+++ src/main/java/nl/wouterdebruijn/slurp/helper/SlurpConfig.java
@@ -31,74 +31,11 @@ public class SlurpConfig {
 
     private static void setValues() {
         FileConfiguration conf = Slurp.plugin.getConfig();
-        if (!conf.contains(ConfigValue.CHANCES_LUCYSTONE.getPath())) { // chances
-            conf.set(ConfigValue.CHANCES_LUCYSTONE.getPath(), 1000000);
-        } else if (!conf.contains(ConfigValue.CHANCES_BURNING_HANDS.getPath())) {
-            conf.set(ConfigValue.CHANCES_BURNING_HANDS.getPath(), 1000000);
-        } else if (!conf.contains(ConfigValue.CHANCES_CHOKING.getPath())) {
-            conf.set(ConfigValue.CHANCES_CHOKING.getPath(), 1000000);
-        } else if (!conf.contains(ConfigValue.CHANCES_FALL_DAMAGE.getPath())) {
-            conf.set(ConfigValue.CHANCES_FALL_DAMAGE.getPath(), 1000000);
-        } else if (!conf.contains(ConfigValue.CHANCES_SPRINTING.getPath())) {
-            conf.set(ConfigValue.CHANCES_SPRINTING.getPath(), 1000000);
-        } else if (!conf.contains(ConfigValue.CHANCES_SWIMMING.getPath())) {
-            conf.set(ConfigValue.CHANCES_SWIMMING.getPath(), 1000000);
-        } else if (!conf.contains(ConfigValue.CHANCES_BOAT.getPath())) {
-            conf.set(ConfigValue.CHANCES_BOAT.getPath(), 1000000);
-        } else if (!conf.contains(ConfigValue.SIP_LUCYSTONE.getPath())) { // sips
-            conf.set(ConfigValue.SIP_LUCYSTONE.getPath(), 0);
-        } else if (!conf.contains(ConfigValue.SIP_BURNING_HANDS.getPath())) {
-            conf.set(ConfigValue.SIP_BURNING_HANDS.getPath(), 5);
-        } else if (!conf.contains(ConfigValue.SIP_CHOKING.getPath())) {
-            conf.set(ConfigValue.SIP_CHOKING.getPath(), 4);
-        } else if (!conf.contains(ConfigValue.SIP_FALL_DAMAGE.getPath())) {
-            conf.set(ConfigValue.SIP_FALL_DAMAGE.getPath(), 2);
-        } else if (!conf.contains(ConfigValue.SIP_DIE.getPath())) {
-            conf.set(ConfigValue.SIP_DIE.getPath(), 2);
-        } else if (!conf.contains(ConfigValue.SIP_KILLER.getPath())) {
-            conf.set(ConfigValue.SIP_KILLER.getPath(), 0);
-        } else if (!conf.contains(ConfigValue.SIP_KILLED_ANIMAL.getPath())) {
-            conf.set(ConfigValue.SIP_KILLED_ANIMAL.getPath(), 3);
-        } else if (!conf.contains(ConfigValue.SIP_SPRINTING.getPath())) {
-            conf.set(ConfigValue.SIP_SPRINTING.getPath(), 2);
-        } else if (!conf.contains(ConfigValue.SIP_SWIMMING.getPath())) {
-            conf.set(ConfigValue.SIP_SWIMMING.getPath(), 2);
-        } else if (!conf.contains(ConfigValue.SIP_BOAT.getPath())) {
-            conf.set(ConfigValue.SIP_BOAT.getPath(), 3);
-        } else if (!conf.contains(ConfigValue.SHOT_LUCYSTONE.getPath())) { //shots
-            conf.set(ConfigValue.SHOT_LUCYSTONE.getPath(), 1);
-        } else if (!conf.contains(ConfigValue.SHOT_BURNING_HANDS.getPath())) {
-            conf.set(ConfigValue.SHOT_BURNING_HANDS.getPath(), 0);
-        } else if (!conf.contains(ConfigValue.SHOT_CHOKING.getPath())) {
-            conf.set(ConfigValue.SHOT_CHOKING.getPath(), 0);
-        } else if (!conf.contains(ConfigValue.SHOT_FALL_DAMAGE.getPath())) {
-            conf.set(ConfigValue.SHOT_FALL_DAMAGE.getPath(), 0);
-        } else if (!conf.contains(ConfigValue.SHOT_DIE.getPath())) {
-            conf.set(ConfigValue.SHOT_DIE.getPath(), 0);
-        } else if (!conf.contains(ConfigValue.SHOT_KILLER.getPath())) {
-            conf.set(ConfigValue.SHOT_KILLER.getPath(), 1);
-        } else if (!conf.contains(ConfigValue.SHOT_KILLED_ANIMAL.getPath())) {
-            conf.set(ConfigValue.SHOT_KILLED_ANIMAL.getPath(), 0);
-        } else if (!conf.contains(ConfigValue.SHOT_SPRINTING.getPath())) {
-            conf.set(ConfigValue.SHOT_SPRINTING.getPath(), 0);
-        } else if (!conf.contains(ConfigValue.SHOT_SWIMMING.getPath())) {
-            conf.set(ConfigValue.SHOT_SWIMMING.getPath(), 0);
-        } else if (!conf.contains(ConfigValue.SHOT_BOAT.getPath())) {
-            conf.set(ConfigValue.SHOT_BOAT.getPath(), 0);
-        }
-    }
-
-    private static void setOreValues() {
-        FileConfiguration conf = Slurp.plugin.getConfig();
-        for (Ores o : Ores.values()) { // set sip value of each ore to 1.
-            if (!conf.contains(o.getPath())) {
-                conf.set(o.getPath(), 1);
-            }
-            if (!conf.contains(o.getChancePath())) {
-                conf.set(o.getChancePath(), o.getDefaultChanceValue());
+        for (ConfigValue cv : ConfigValue.values()) {
+            if (!conf.contains(cv.getPath())) {
+                conf.set(cv.getPath(), cv.getDefaultValue());
             }
         }
-
     }
 
     /**
@@ -125,6 +62,34 @@ public class SlurpConfig {
         FileConfiguration conf = Slurp.plugin.getConfig();
         conf.set(val.getPath(), value);
         saveConfig();
+    }
+
+    private static void setOreValues() {
+        FileConfiguration conf = Slurp.plugin.getConfig();
+        for (Ores o : Ores.values()) { // set sip value of each ore to 1.
+            if (!conf.contains(o.getPath())) {
+                conf.set(o.getPath(), 1);
+            }
+            if (!conf.contains(o.getChancePath())) {
+                conf.set(o.getChancePath(), o.getDefaultChanceValue());
+            }
+        }
+    }
+
+    public static int getOreSipValue(Ores o) {
+        FileConfiguration conf = Slurp.plugin.getConfig();
+        if (conf.contains(o.getPath())) {
+            return conf.getInt(o.getPath());
+        }
+        return 0;
+    }
+
+    public static int getOreChanceValue(Ores o) {
+        FileConfiguration conf = Slurp.plugin.getConfig();
+        if (conf.contains(o.getChancePath())) {
+            return conf.getInt(o.getChancePath());
+        }
+        return 0;
     }
 
     public static void saveConfig() {

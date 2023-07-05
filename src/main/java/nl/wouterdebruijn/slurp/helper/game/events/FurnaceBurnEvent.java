d4b2ef842269866a438ee9d4fcb030298f0ce7fa
Wouter de Bruijn <wouter@hedium.nl>
2023-07-05 21:18:26 +0200

    ✨ Added Furnace Burn event

---
+++ src/main/java/nl/wouterdebruijn/slurp/helper/game/events/FurnaceBurnEvent.java
@@ -0,0 +1,43 @@
+package nl.wouterdebruijn.slurp.helper.game.events;
+
+import nl.wouterdebruijn.slurp.Slurp;
+import nl.wouterdebruijn.slurp.helper.RandomGenerator;
+import nl.wouterdebruijn.slurp.helper.game.entity.SlurpPlayer;
+import nl.wouterdebruijn.slurp.helper.game.manager.SlurpPlayerManager;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.FurnaceExtractEvent;
+
+public class FurnaceBurnEvent extends GameEvent implements Listener {
+    public FurnaceBurnEvent(FileConfiguration config) {
+        super(config, "furnace-burn");
+    }
+
+    @Override
+    public GameEvent register(Slurp plugin) {
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
+        return this;
+    }
+
+    @EventHandler
+    public void onFurnaceBurn(FurnaceExtractEvent event) {
+        Player player = event.getPlayer();
+        SlurpPlayer slurpPlayer = SlurpPlayerManager.getPlayer(player);
+
+        int amount = event.getItemAmount();
+
+        if (SlurpPlayerManager.checkNull(player, slurpPlayer)) {
+            return;
+        }
+
+        // The change is divided by the amount of items, so that the chance is the same for every item in the stack
+        float calculatedChance = ((float) this.getChance() / amount);
+
+        if (!RandomGenerator.hasChance((int) Math.ceil(calculatedChance)))
+            return;
+
+        this.triggerFor(slurpPlayer);
+    }
+}

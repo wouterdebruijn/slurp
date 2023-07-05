a8f6d568cf95dbd7bf48679085393f6ef07b5630
Wouter de Bruijn <wouter@hedium.nl>
2023-07-05 22:11:58 +0200

    ✨ Added food choking event

---
+++ src/main/java/nl/wouterdebruijn/slurp/helper/game/events/ChokingPlayerEvent.java
@@ -0,0 +1,46 @@
+package nl.wouterdebruijn.slurp.helper.game.events;
+
+import nl.wouterdebruijn.slurp.Slurp;
+import nl.wouterdebruijn.slurp.helper.RandomGenerator;
+import nl.wouterdebruijn.slurp.helper.game.entity.SlurpPlayer;
+import nl.wouterdebruijn.slurp.helper.game.manager.SlurpPlayerManager;
+import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.player.PlayerItemConsumeEvent;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ChokingPlayerEvent extends GameEvent implements Listener {
+    private static final ArrayList<Material> materials = new ArrayList<>(Arrays.asList(Material.STONE, Material.DEEPSLATE, Material.DIORITE, Material.GRANITE, Material.ANDESITE));
+    public ChokingPlayerEvent(FileConfiguration config) {
+        super(config, "choking-player");
+    }
+
+    @Override
+    public GameEvent register(Slurp plugin) {
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
+        return this;
+    }
+
+    @EventHandler
+    public void onEvent(PlayerItemConsumeEvent event) {
+        Player player = event.getPlayer();
+        SlurpPlayer slurpPlayer = SlurpPlayerManager.getPlayer(player);
+
+        if (SlurpPlayerManager.checkNull(player, slurpPlayer)) {
+            return;
+        }
+
+        // Check if the player has a chance to trigger the event
+        if (!RandomGenerator.hasChance(this.getChance()))
+            return;
+
+        // Trigger the event
+        this.triggerFor(slurpPlayer);
+    }
+}

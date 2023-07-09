7b9d0144ff0ab19e3559a682d55481e5c2e91c1c
Wouter de Bruijn <wouter@hedium.nl>
2023-07-09 11:21:02 +0200

    ✨ Added shot on player death

---
+++ src/main/java/nl/wouterdebruijn/slurp/helper/game/events/PlayerDiesEvent.java
@@ -0,0 +1,46 @@
+package nl.wouterdebruijn.slurp.helper.game.events;
+
+import nl.wouterdebruijn.slurp.Slurp;
+import nl.wouterdebruijn.slurp.helper.RandomGenerator;
+import nl.wouterdebruijn.slurp.helper.game.entity.SlurpPlayer;
+import nl.wouterdebruijn.slurp.helper.game.manager.SlurpPlayerManager;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+public class PlayerDiesEvent extends GameEvent implements Listener {
+    public PlayerDiesEvent(FileConfiguration config) {
+        super(config, "player-death");
+    }
+
+    @Override
+    public GameEvent register(Slurp plugin) {
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
+        return this;
+    }
+
+    @EventHandler
+    public void onEvent(PlayerDeathEvent event) {
+        Player player = event.getPlayer();
+        SlurpPlayer slurpPlayer = SlurpPlayerManager.getPlayer(player);
+
+        if (SlurpPlayerManager.checkNull(player, slurpPlayer)) {
+            return;
+        }
+
+        // Check if the player has a killer
+        if (player.getKiller() == null) {
+            return;
+        }
+
+        // Check if the player has a chance to trigger the event
+        if (!RandomGenerator.hasChance(this.getChance()))
+            return;
+
+        // Trigger the event
+        this.triggerFor(slurpPlayer);
+    }
+}

1559ed7e8a9ff02332667b60e60fba626c670f83
Wouter de Bruijn <wouter@hedium.nl>
2023-07-09 11:16:41 +0200

    ✨ Added Broken legs event

---
+++ src/main/java/nl/wouterdebruijn/slurp/helper/game/events/BrokenLegsEvent.java
@@ -0,0 +1,46 @@
+package nl.wouterdebruijn.slurp.helper.game.events;
+
+import nl.wouterdebruijn.slurp.Slurp;
+import nl.wouterdebruijn.slurp.helper.RandomGenerator;
+import nl.wouterdebruijn.slurp.helper.game.entity.SlurpPlayer;
+import nl.wouterdebruijn.slurp.helper.game.manager.SlurpPlayerManager;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+
+public class BrokenLegsEvent extends GameEvent implements Listener {
+    public BrokenLegsEvent(FileConfiguration config) {
+        super(config, "broken-legs");
+    }
+
+    @Override
+    public GameEvent register(Slurp plugin) {
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
+        return this;
+    }
+
+    @EventHandler
+    public void onEvent(EntityDamageEvent event) {
+        if (!(event.getEntity() instanceof Player player))
+            return;
+
+        SlurpPlayer slurpPlayer = SlurpPlayerManager.getPlayer(player);
+
+        if (SlurpPlayerManager.checkNull(player, slurpPlayer)) {
+            return;
+        }
+
+        // Check if the player got fall damage
+        if (!event.getCause().equals(EntityDamageEvent.DamageCause.FALL))
+            return;
+
+        // Check if the player has a chance to trigger the event
+        if (!RandomGenerator.hasChance(this.getChance()))
+            return;
+
+        // Trigger the event
+        this.triggerFor(slurpPlayer);
+    }
+}

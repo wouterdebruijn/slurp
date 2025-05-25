c07b97758f45b0698bcfda763a9de9afa14a8e51
Wouter de Bruijn <wouter@hedium.nl>
2025-05-25 16:29:43 +0200

    🔧 Config value changes for new variables

--- src/main/java/nl/wouterdebruijn/slurp/helper/ConfigValue.java
+++ src/main/java/nl/wouterdebruijn/slurp/helper/ConfigValue.java
@@ -2,7 +2,9 @@ package nl.wouterdebruijn.slurp.helper;
 
 public enum ConfigValue {
     DIFFICULTY_MULTIPLIER("game.difficulty-multiplier"),
-    SIP_SHOT_RATIO("game.sip-shot-ratio");
+    SIP_SHOT_RATIO("game.sip-shot-ratio"),
+    GENERATION_INTERVAL("game.generation-interval"),
+    MINIMAL_RARITY("game.minimal-rarity");
 
     private final String confPath;
 
